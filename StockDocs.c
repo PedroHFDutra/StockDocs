825230308df949d84ef4026dcf5d98147d04d998
Pedro Henrique <106541103+PedroHFDutra@users.noreply.github.com>
2022-07-22 12:45:35 -0300

    Update StockDocs.c

--- StockDocs.c
+++ StockDocs.c
@@ -1,1 +1,29 @@
+#include <stdin.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <locale.h>
 
+struct cadastro_estoque {
+char nome_produto[999];
+char lote_produto[999];
+int codigo_produto;
+};
+
+struct cadastro_funcionario {
+char nome_funcionario[999];
+char cpf[999];
+
+
+
+};
+
+
+int main(int argc, char *argv[]) {
+    setLOCALE(LC_ALL, "Portuguese");
+
+
+
+
+    return 0;
+}
